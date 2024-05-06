fda91d8454c9def67f2d4043e4115144c0ec1d82
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-05-05 18:17:09 -0600

    Update RAM.h

--- src/RAM.h
+++ src/RAM.h
@@ -4,3 +4,13 @@
 void Free(int* ptr) {
     free(ptr);
 }
+
+void GiveTo(const char file, int bytes) {
+    #define FILE = fopen(file, "rw");
+
+    if (file == NULL) {
+        perror("Error opening file: ");
+    } else {
+        int* ptr = (int*)malloc(bytes * sizeof(int));
+    }
+}
